d6ae152d6c5ff0bba95022c9879333bac49947c8
Cesar Vargas <limitlis@gmail.com>
2026-04-30 12:18:44 -0600

    chore: Update package.json to fit the updated package format

--- website/src/components.d.ts
+++ website/src/components.d.ts
@@ -1,9 +1,18 @@
-
 declare module '*?raw' {
-  const content: string;
-  export default content;
+    const content: string;
+    export default content;
 }
 declare module '*.css' {
-  const content: string;
-  export default content;
+    const content: string;
+    export default content;
+}
+
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue';
+    const component: DefineComponent<{}, {}, any>;
+    export default component;
+    export interface GlobalComponents {
+        PsodaFizz: typeof PsodaFizz;
+        PsodaPop: typeof PsodaPop;
+    }
 }
