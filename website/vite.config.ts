6f588bed81888977395011715e353667264709ce
Cesar Vargas <limitlis@gmail.com>
2026-04-30 07:14:17 -0600

    fix: Update base path in website vite config

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/psoda-vue/',
     staged: {
         '*': 'vp check --fix',
   },
